98cdfb04fafe9f927700528db3aa8e86488a940c
Adelleye <adelleye@yahoo.com>
2019-08-26 12:13:40 -0400

    Editing Header

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,18 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div>
+    <div className="HeaderGroup">
+      <Link to="/about">About</Link>
+      <Link to="/ourInstructors">Our Instructors</Link>
+      <Link to="/classes">Classes</Link>
+      <Link to="/location">Location</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/bookAClass">
+        <button>Book a Class</button>
+      </Link>
     </div>
-  </header>
+  </div>
 )
 
 Header.propTypes = {
