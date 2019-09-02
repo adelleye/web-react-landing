2d03391e91251880bf0175027039f4db3f6769bd
Adelleye <adelleye@yahoo.com>
2019-09-02 10:48:54 -0400

    updated second content

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,54 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  //When a component is created, the constructor will be called.
+  constructor(props) {
+    super(props)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    this.state = {
+      hasScrolled: false,
+    }
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  /*As soon as the component is loaded, we try to listen to the built-in scroll events passed from our window. This will in turn trigger the handleScroll function*/
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  /*The handleScroll function will get the Y position of the scrolling. When the scrolling hits more than 50px, it’ll set a new state for our hasScrolled boolean to true. Otherwise, it’ll set it back to false.*/
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      >
+        <div id="site-title" align="left">
+          <a>Colima</a>
+        </div>
+        <div className="HeaderGroup">
+          <Link to="/about">About</Link>
+          <Link to="/ourInstructors">Our Instructors</Link>
+          <Link to="/classes">Classes</Link>
+          <Link to="/location">Location</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/bookAClass">
+            <button>Book a Class</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
